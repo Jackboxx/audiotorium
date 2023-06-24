c1e630222a6c1204ee99fb67dc4ef173d4a8e451
jackboxx <lucygschwantner@gmail.com>
2023-06-24 21:30:31 +0200

    add helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#![allow(dead_code)]
+
+use std::fs::File;
+use std::io::BufReader;
+use std::process::Command;
+
 use cpal::traits::{DeviceTrait, HostTrait};
 use home_audio_manager::rpc_source_service_server::{RpcSourceService, RpcSourceServiceServer};
 use home_audio_manager::{Empty, GetSourcesResponse};
+use rodio::Decoder;
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod home_audio_manager {
@@ -27,6 +34,29 @@ impl RpcSourceService for SourceService {
 
         Ok(Response::new(res))
     }
+}
+
+fn get_audio_from_file(title: &str, url: &str) -> Result<Decoder<BufReader<File>>, anyhow::Error> {
+    let path = format!("audio/{title}");
+    let file = match File::open(&path) {
+        Ok(file) => file,
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                download_audio(url, &path)?;
+                File::open(title)?
+            }
+            _ => return Err(err.into()),
+        },
+    };
+
+    Ok(Decoder::new(BufReader::new(file))?)
+}
+
+fn download_audio(url: &str, download_location: &str) -> anyhow::Result<()> {
+    Command::new("yt-dlp")
+        .args(["-x", "--bestaudio", &format!("-o {download_location}"), url])
+        .output()?;
+    Ok(())
 }
 
 #[tokio::main]
