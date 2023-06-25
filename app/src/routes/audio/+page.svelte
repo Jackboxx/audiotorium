69b85b4ca11d38e78f40903a499bd0bd40a71ac2
jackboxx <lucygschwantner@gmail.com>
2023-06-25 21:15:04 +0200

    add search bar

--- app/src/routes/audio/+page.svelte
+++ app/src/routes/audio/+page.svelte
@@ -1,1 +1,11 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import SearchBar from '../../lib/SearchBar.svelte';
+	let searchText = '';
+</script>
+
+<div class="flex min-h-screen w-full flex-col items-center bg-gray-100 dark:bg-zinc-900">
+	<SearchBar
+		bind:searchText
+		onConfirm={() => console.log(`current text: ${searchText}`)}
+	/>
+</div>
