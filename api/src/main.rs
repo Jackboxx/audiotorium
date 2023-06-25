82ce60d6af875868129c5684a44dd8ac56744903
Jackboxx <gschwantnermoritz@gmail.com>
2023-06-25 16:53:47 +0200

    switch to rest api instead of protobufs

--- api/src/main.rs
+++ api/src/main.rs
@@ -4,36 +4,24 @@ use std::fs::File;
 use std::io::BufReader;
 use std::process::Command;
 
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use cpal::traits::{DeviceTrait, HostTrait};
-use home_audio_manager::rpc_source_service_server::{RpcSourceService, RpcSourceServiceServer};
-use home_audio_manager::{Empty, GetSourcesResponse};
 use rodio::Decoder;
-use tonic::{transport::Server, Request, Response, Status};
 
-pub mod home_audio_manager {
-    tonic::include_proto!("home_audio_manager");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_sources))
+        .bind(("127.0.0.1", 50051))?
+        .run()
+        .await
 }
 
-#[derive(Debug, Default)]
-pub struct SourceService;
+#[get("/sources")]
+async fn get_sources() -> impl Responder {
+    let host = cpal::default_host();
+    let devices = host.output_devices().unwrap();
 
-#[tonic::async_trait]
-impl RpcSourceService for SourceService {
-    async fn get_sources(
-        &self,
-        request: Request<Empty>,
-    ) -> Result<Response<GetSourcesResponse>, Status> {
-        println!("Got a request: {:?}", request);
-
-        let host = cpal::default_host();
-        let devices = host.output_devices().unwrap();
-
-        let res = GetSourcesResponse {
-            names: devices.flat_map(|dev| dev.name()).collect(),
-        };
-
-        Ok(Response::new(res))
-    }
+    HttpResponse::Ok().json(devices.flat_map(|dev| dev.name()).collect::<Vec<_>>())
 }
 
 fn get_audio_from_file(title: &str, url: &str) -> Result<Decoder<BufReader<File>>, anyhow::Error> {
@@ -56,18 +44,5 @@ fn download_audio(url: &str, download_location: &str) -> anyhow::Result<()> {
     Command::new("yt-dlp")
         .args(["-x", "--bestaudio", &format!("-o {download_location}"), url])
         .output()?;
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    let addr = "[::1]:50051".parse()?;
-    let manager = SourceService::default();
-
-    Server::builder()
-        .add_service(RpcSourceServiceServer::new(manager))
-        .serve(addr)
-        .await?;
-
     Ok(())
 }
