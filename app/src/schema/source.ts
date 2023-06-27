d5a0b1b7c2c2d8cca4002b035bfa41ac49233426
jackboxx <lucygschwantner@gmail.com>
2023-06-27 22:24:46 +0200

    add title to youtube videos and format

--- app/src/schema/source.ts
+++ app/src/schema/source.ts
@@ -1,4 +1,4 @@
-import {z} from "zod";
+import { z } from 'zod';
 
 export const GetSourcesSchema = z.string().array();
 export type GetSources = z.infer<typeof GetSourcesSchema>;
--- app/src/schema/video.ts
+++ app/src/schema/video.ts
@@ -1,24 +1,26 @@
-import {PUBLIC_YOUTUBE_API_KEY} from "$env/static/public";
-import {z} from "zod";
+import { PUBLIC_YOUTUBE_API_KEY } from '$env/static/public';
+import { z } from 'zod';
 
-export const ThumbNailSchema =
-    z.object({url : z.string().url(), width : z.number(), height : z.number()});
+export const ThumbNailSchema = z.object({
+	url: z.string().url(),
+	width: z.number(),
+	height: z.number()
+});
 
 export const ThumbNailsSchema = z.object({
-  default : ThumbNailSchema,
-  medium : ThumbNailSchema.nullish(),
-  high : ThumbNailSchema.nullish(),
-})
+	default: ThumbNailSchema,
+	medium: ThumbNailSchema.nullish(),
+	high: ThumbNailSchema.nullish()
+});
 
 export const YouTubeVideoSchema = z.object({
-  id : z.object({
-    videoId : z.string().nonempty(),
-  }),
-  snippet : z.object({thumbnails : ThumbNailsSchema})
-})
+	id: z.object({
+		videoId: z.string().nonempty()
+	}),
+	snippet: z.object({ title: z.string().nonempty(), thumbnails: ThumbNailsSchema })
+});
 
-export const YouTubeSearchResultSchema =
-    z.object({items : YouTubeVideoSchema.array()})
+export const YouTubeSearchResultSchema = z.object({ items: YouTubeVideoSchema.array() });
 
 export type ThumbNail = z.infer<typeof ThumbNailSchema>;
 export type ThumbNails = z.infer<typeof ThumbNailsSchema>;
@@ -26,15 +28,15 @@ export type YouTubeVideo = z.infer<typeof YouTubeVideoSchema>;
 export type YouTubeSearchResult = z.infer<typeof YouTubeSearchResultSchema>;
 
 export function getBestThumbNail(thumbNails: ThumbNails): ThumbNail {
-  return thumbNails.high ?? thumbNails.medium ?? thumbNails.default;
+	return thumbNails.high ?? thumbNails.medium ?? thumbNails.default;
 }
 
 export function getVideoUrl(video: YouTubeVideo): string {
-  return `https://www.youtube.com/watch?v=${video.id.videoId}`
+	return `https://www.youtube.com/watch?v=${video.id.videoId}`;
 }
 
 export async function searchForVideos(search: string): Promise<Response> {
-  return await fetch(`https://www.googleapis.com/youtube/v3/search?q=${
-      search}&part=snippet&type=video&maxResults=10&key=${
-      PUBLIC_YOUTUBE_API_KEY}`);
+	return await fetch(
+		`https://www.googleapis.com/youtube/v3/search?q=${search}&part=snippet&type=video&maxResults=10&key=${PUBLIC_YOUTUBE_API_KEY}`
+	);
 }
