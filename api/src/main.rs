f905551decec785020066ce214bb2f32c2483638
jackboxx <lucygschwantner@gmail.com>
2023-07-21 15:52:19 +0200

    help

--- api/src/main.rs
+++ api/src/main.rs
@@ -18,7 +18,11 @@ mod downloader;
 mod server;
 mod session;
 
-pub static AUDIO_DIR: &str = "audio";
+pub static AUDIO_DIR: &str = if cfg!(not(debug_assertions)) {
+    "/home/lucifer/audio"
+} else {
+    "audio"
+};
 
 pub struct AppData {
     queue_server_addr: Addr<QueueServer>,
