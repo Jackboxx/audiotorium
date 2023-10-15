182d44805abbc02df9a16615a4fa8b2867eba678
jackboxx <lucygschwantner@gmail.com>
2023-10-15 12:24:53 +0200

    dummys everywhere

--- audio-manager/fake-alsa/src/main.rs
+++ audio-manager/fake-alsa/src/main.rs
@@ -1,12 +1,10 @@
 use std::{env, fs, path::PathBuf};
 
-const LOOPBACK_CARD_ID: u8 = 3;
-
 fn main() {
     let entries = (0..3)
-        .map(|i| generate_asoundrc_entry(&format!("dummy_{i}"), LOOPBACK_CARD_ID))
+        .map(|i| generate_asoundrc_entry(i))
         .collect::<Vec<_>>()
-        .join("\n");
+        .join("\n\n");
 
     let home = env::var("HOME").expect("HOME env var should exits");
     let path = PathBuf::from(home).join(".asoundrc");
@@ -14,20 +12,19 @@ fn main() {
     fs::write(path, entries).expect("should be able to write to $HOME/.asoundrc");
 }
 
-fn generate_asoundrc_entry(name: &str, loopback_card_id: u8) -> String {
+fn generate_asoundrc_entry(index: u8) -> String {
     format!(
-        r#"
-pcm.{name} {{
+        r#"pcm.dummy_{index} {{
         type plug
         slave {{
-                pcm "dmix:PCH,{loopback_card_id}"
+                pcm "hw:Loopback,0,{index}"
         }}
         hint {{
                 show on
                 description "dummy device"
         }}
 }}
-ctl.{name} {{
+ctl.dummy_{index} {{
         type dmix
 }}"#
     )
